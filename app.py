32b45114c5affd5a4fecf1246fb857e59fd0d736
C K Manoj <manojck012@gmail.com>
2021-12-12 14:05:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,redirect,url_for,request,render_template
+import numpy as np
+import pickle
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl","rb"))
+
+
+@app.route("/")
+def home():
+    return render_template('input.html')
+   
+@app.route("/predict",methods=["POST"])
+def prediction():
+    int_features = [float(x) for x in request.form.values()]   
+    
+    sc = pickle.load(open("standardScalar.pkl","rb"))
+    trans_features = sc.transform([[float(x) for x in request.form.values()]])
+
+    predictions = model.predict(trans_features)
+    return render_template("input.html",salary = "The expected Salary is  ${}".format(round(predictions[0])))   
+    
+  #  sc = StandardScaler()
+  #  predictions = model.predict(final_pred)
+
+    #return render_template("input.html",salary = "The expected Salary is  ${}".format(round(predictions[0],0)))   
+
+
+if __name__ == "__main__":
+        app.run(debug = True)
+
